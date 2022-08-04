60fd3edfa8a80ef04010bb7f4392a9e72c8af564
recipra <jmo0425@outlook.com>
2022-08-04 19:41:18 -0400

    add edit and delete duck functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('ducks/', views.ducks_index, name='ducks_index'),
   path('ducks/<int:duck_id>/', views.ducks_detail, name='ducks_detail'),
-  path('ducks/create/', views.DuckCreate.as_view(), name='ducks_create')
+  path('ducks/create/', views.DuckCreate.as_view(), name='ducks_create'),
+  path('ducks/<int:pk>/update/', views.DuckUpdate.as_view(), name='ducks_update'),
+  path('ducks/<int:pk>/delete/', views.DuckDelete.as_view(), name='ducks_delete')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Duck
 
 # Create your views here.
@@ -20,3 +20,11 @@ def ducks_detail(request, duck_id):
 class DuckCreate(CreateView):
   model = Duck
   fields = '__all__'
+
+class DuckUpdate(UpdateView):
+  model = Duck
+  fields = ['description', 'quack_snack', 'cool']
+
+class DuckDelete(DeleteView):
+  model = Duck
+  success_url = '/ducks/'
