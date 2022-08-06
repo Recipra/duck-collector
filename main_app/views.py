b9589b88ba60712cc97129e5ca1abfa253a784a5
recipra <jmo0425@outlook.com>
2022-08-06 14:00:24 -0400

    complete lab

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
@@ -29,16 +31,19 @@ class Home(LoginView):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def ducks_index(request):
-  ducks = Duck.objects.all()
+  ducks = Duck.objects.filter(user=request.user)
   return render(request, 'ducks/index.html', {'ducks': ducks})
 
+@login_required
 def ducks_detail(request, duck_id):
   duck = Duck.objects.get(id=duck_id)
   hats_duck_doesnt_have = Hat.objects.exclude(id__in = duck.hats.all().values_list('id'))
   pr_form = PRForm()
   return render(request, 'ducks/detail.html', {'duck': duck, 'pr_form': pr_form, 'hats': hats_duck_doesnt_have})
 
+@login_required
 def add_deed(request, duck_id):
   form = PRForm(request.POST)
   if form.is_valid():
@@ -47,15 +52,17 @@ def add_deed(request, duck_id):
     new_deed.save()
   return redirect('ducks_detail', duck_id=duck_id)
 
+@login_required
 def assoc_hat(request, duck_id, hat_id):
   Duck.objects.get(id=duck_id).hats.add(hat_id)
   return redirect('ducks_detail', duck_id=duck_id)
 
+@login_required
 def remove_hat(request, duck_id, hat_id):
   Duck.objects.get(id=duck_id).hats.remove(hat_id)
   return redirect('ducks_detail', duck_id=duck_id)
 
-class DuckCreate(CreateView):
+class DuckCreate(LoginRequiredMixin, CreateView):
   model = Duck
   fields = ['name', 'description', 'quack_snack', 'cool']
 
@@ -63,28 +70,28 @@ class DuckCreate(CreateView):
     form.instance.user = self.request.user
     return super().form_valid(form)
 
-class DuckUpdate(UpdateView):
+class DuckUpdate(LoginRequiredMixin, UpdateView):
   model = Duck
   fields = ['description', 'quack_snack', 'cool']
 
-class DuckDelete(DeleteView):
+class DuckDelete(LoginRequiredMixin, DeleteView):
   model = Duck
   success_url = '/ducks/'
 
-class HatCreate(CreateView):
+class HatCreate(LoginRequiredMixin, CreateView):
   model = Hat
   fields = '__all__'
 
-class HatList(ListView):
+class HatList(LoginRequiredMixin, ListView):
   model = Hat
 
-class HatDetail(DetailView):
+class HatDetail(LoginRequiredMixin, DetailView):
   model = Hat
 
-class HatUpdate(UpdateView):
+class HatUpdate(LoginRequiredMixin, UpdateView):
   model = Hat
   fields = ['type', 'color']
 
-class HatDelete(DeleteView):
+class HatDelete(LoginRequiredMixin, DeleteView):
   model = Hat
   success_url = '/hats/'
